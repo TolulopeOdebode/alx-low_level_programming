cdd2452603d2615544a060e3339eea029589f944
TolulopeOdebode <odebodetolulopetosin@gmail.com>
2022-06-17 04:43:54 +0000

    commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - This will test the functions that prints if interger is positive or negative
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	posiive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * main - Largest number returns the largest of three numbers
+ * @x: first integer
+ * @y: second integer
+ * @z: third integer
+ * Return: largest number
+ */
+int largest_number(int x, int y, int z)
+{
+	int largest;
+
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
+}
